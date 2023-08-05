881295b708dd5a9ee868ad24f17e698ad12a438b
elena <elenastarck1992@mail.ru>
2023-08-05 11:27:57 +0300

    foo title_letter added

--- str_func.py
+++ str_func.py
@@ -1,2 +1,6 @@
 def upper_letters(words: str):
     return words.upper()
+
+def title_letter(words: str):
+    """Принимает строку. Возвращает эту строку с первой заглавной буквой"""
+    return words.title()
